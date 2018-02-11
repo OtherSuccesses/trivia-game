d41b3033fc6fbae050a2870a4f6729b733487a2e
OtherSuccesses <viskaslietuvai@gmail.com>
2018-02-11 16:19:45 -0500

    questions seeding

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,219 +1,266 @@
- game = {
+var interval;
+
+var game = {
 	"currentQuestion":{
 	},
 	"right":0,
-	"wrong":0,
+	"incorrect":0,
 	"blank":0,
 	"progress":0,
 	"timer":30,
-	"easyQuestions":{
-		"questionOne":{
+
+	//All the Questions
+	"easyQuestions":[
+		{
 			"question": "What are non-magical people called?",
 			"chosen": false,
 			"correct": "Muggles",
-			"wrongOne": "Boggles",
-			"wrongTwo": "Figgs",
-			"wrongThree": "Bludgers",
-			"image": "assets/images/Muggle.jps",
+			"wrong": ["Boggles", "Figgs", "Bludgers"],
+			"image": "assets/images/Muggle.jpg"
 		},
-		"questionTwo":{
+		{
 			"question": "What is Harry's cousin's name?",
 			"chosen": false,
 			"correct": "Dudley",
-			"wrongOne": "Didley",
-			"wrongTwo": "Ramsey",
-			"wrongThree": "Sam",
-			"image": "assets/images/dudley_400x400.jpg",
+			"wrong": ["Didley", "Ramsey", "Sam"],
+			"image": "assets/images/dudley_400x400.jpg"
 
 		},
-		"questionThree":{
+		{
 			"question": "What position in quidditch does Harry play?",
 			"chosen": false,
 			"correct": "Seeker",
-			"wrongOne": "Keeper",
-			"wrongTwo": "Chaser",
-			"wrongThree": "Center",
-			"image": "assets/images/Quidditch.jpg",
+			"wrong": ["Keeper", "Chaser", "Center"],
+			"image": "assets/images/Quidditch.jpg"
 
 		},
-		"questionFour":{
+		{
 			"question": "What club could Nearly Headless Nick never join?",
 			"chosen": false,
 			"correct": "The Headless Hunt",
-			"wrongOne": "The Poltergeists",
-			"wrongTwo": "The Hallowed Hall",
-			"wrongThree": "The Gallant Ghosts",
-			"image": "assets/images/Nearly_Headless_Nick.jpg",
+			"wrong": ["The Poltergeists", "The Hallowed Hall", "The Gallant Ghosts"],
+			"image": "assets/images/Nearly_Headless_Nick.jpg"
 
 		},
-		"questionFive":{
+		{
 			"question": "What animal represents Hufflepuff?",
 			"chosen": false,
 			"correct": "Badger",
-			"wrongOne": "Lion",
-			"wrongTwo": "Snake",
-			"wrongThree": "Armadillo",
-			"image": "assets/images/Hufflepuff.jpg",
+			"wrong": ["Lion", "Snake", "Armadillo"],
+			"image": "assets/images/Hufflepuff.jpg"
 
 		},
-		"questionSix":{
+		{
 			"question": "Who is Albus Dumbledore?",
 			"chosen": false,
 			"correct": "The Headmaster at Hogwarts",
-			"wrongOne": "The Minister of Magic",
-			"wrongTwo": "The wizened janitor",
-			"wrongThree": "An inventor",
-			"image": "assets/images/Albus_Dumbledore.jpg",
+			"wrong": ["The Minister of Magic", "The wizened janitor", "An inventor"],
+			"image": "assets/images/Albus_Dumbledore.jpg"
 
 		},
-		"questionSeven":{
+		{
 			"question": "What is a Squib",
 			"chosen": false,
 			"correct": "Someone who should be able to do magic but can't.",
-			"wrongOne": "A small explosion.",
-			"wrongTwo": "A broken wand.",
-			"wrongThree": "A play in Quidditch",
+			"wrong": ["A small explosion.", "A broken wand.", "A play in Quidditch"],
 			"image": "assets/images/walder-frey-34.jpg",
 
 		},
-		"questionEight":{
+		{
 			"question": "What is Hedwig?",
 			"chosen": false,
 			"correct": "An Owl",
-			"wrongOne": "A rat",
-			"wrongTwo": "A cat",
-			"wrongThree": "A vampire",
+			"wrong": ["A rat", "A cat", "A vampire"],
 			"image": "assets/images/Hedwig.jpg",
 
-		}
-	},
-	"mediumQuestions":{
-		"questionNine":{
+		}],
+	"mediumQuestions":[
+		{
 			"question": "What was Dobby given that granted him his freedom?",
 			"chosen": false,
 			"correct": "A sock",
-			"wrongOne": "A ring",
-			"wrongTwo": "A pillow",
-			"wrongThree": "Pants",
+			"wrong": ["A ring", "A pillow", "Pants"],
 			"image": "assets/images/1294516396126_f.jpg",
 
 		},
-		"questionTen":{
+		{
 			"question": "What was Voldemort's middle name?",
 			"chosen": false,
 			"correct": "Marvolo",
-			"wrongOne": "Tom",
-			"wrongTwo": "Riddle",
-			"wrongThree": "Salazar",
+			"wrong": ["Tom", "Riddle", "Salazar"],
 			"image": "assets/images/Tom_Riddle.jpg",
 
 		},
-		"questionEleven":{
+		{
 			"question": "What does Harry use to breathe underwater?",
 			"chosen": false,
 			"correct": "Gillyweed",
-			"wrongOne": "Bezoar",
-			"wrongTwo": "Bubble Kelp",
-			"wrongThree": "Thestral",
+			"wrong": ["Bezoar", "Bubble Kelp", "Thestral"],
 			"image": "assets/images/Gillyweed_raw.jpg",
 
 		},
-		"questionTwelve":{
+		{
 			"question": "What is Peeves?",
 			"chosen": false,
 			"correct": "A poltergeist",
-			"wrongOne": "A giant dog",
-			"wrongTwo": "A talking hat",
-			"wrongThree": "A cat that runs a shop",
+			"wrong": ["A giant dog",  "A talking hat", "A cat that runs a shop"],
 			"image": "assets/images/Peeves01",
 
 		},
-		"questionThirteen":{
+		{
 			"question": "What kind of creatures does Charlie Weasley work with in Romania",
 			"chosen": false,
 			"correct": "Dragons",
-			"wrongOne": "Banshees",
-			"wrongTwo": "Mermaids",
-			"wrongThree": "Unicorns",
+			"wrong": ["Banshees", "Mermaids", "Unicorns"],
 			"image": "assets/images/dragon.jpg",
 
 		},
-		"questionFourteen":{
+		{
 			"question": "What is the name of the Weasley brothers' business venture",
 			"chosen": false,
 			"correct": "Weasley Wizard Wheezes",
-			"wrongOne": "Weasley Wondrous Whizbangs",
-			"wrongTwo": "Magical Marvels Market",
-			"wrongThree": "The Magic Jokeshop",
+			"wrong": ["Weasley Wondrous Whizbangs", "Magical Marvels Market", "The Magic Jokeshop"],
 			"image": "assets/images/WWW.jpg",
 
 		},
-		"questionFifteen":{
+		{
 			"question": "Which of these is not an Unforgivable Curse",
 			"chosen": false,
 			"correct": "Sectumsempra",
-			"wrongOne": "Avada Kedavra",
-			"wrongTwo": "Cruciatus",
-			"wrongThree": "Imperius",
+			"wrong": ["Avada Kedavra", "Cruciatus", "Imperius"],
 			"image": "assets/images/The_Unforgivable_Curses.jpg",
 
 		},
-		"questionSixteen":{
+		{
 			"question": "What does Ron see in the Mirror of Erised",
 			"chosen": false,
 			"correct": "Himself holding up the Quidditch Cup",
-			"wrongOne": "Kissing Hermione",
-			"wrongTwo": "How to finally defeat Voldemort",
-			"wrongThree": "Being wealthy",
+			"wrong": ["Kissing Hermione", "How to finally defeat Voldemort", "Being wealthy"],
 			"image": "assets/images/RonErised.jpg",
 
-		}
-	},
-	"hardQuestions":{
-		"questionSeventeen":{
+		}],
+	"hardQuestions":[
+		{
 			"question": "What form does Snape's patronus take?",
 			"chosen": false,
 			"correct": "Doe",
-			"wrongOne": "Stag",
-			"wrongTwo": "Bat",
-			"wrongThree": "Iguana",
+			"wrong": ["Stag", "Bat", "Iguana"],
 			"image": "assets/images/Snape_patronus.jpg",
 
 		},
-		"questionEighteen":{
+		{
 			"question": "Which of these are not used by Ollivander in wand making?",
 			"chosen": false,
 			"correct": "Veela Hair",
-			"wrongOne": "Dragon Heartstring",
-			"wrongTwo": "Unicorn Hair",
-			"wrongThree": "Phoenix Feather",
+			"wrong": ["Dragon Heartstring", "Unicorn Hair", "Phoenix Feather"],
 			"image": "assets/images/wands.jpg",
 
 		},
-		"questionNineteen":{
+		{
 			"question": "What year was Harry Potter born?",
 			"chosen": false,
 			"correct": "1980",
-			"wrongOne": "1997",
-			"wrongTwo": "2001",
-			"wrongThree": "1986",
+			"wrong": ["1997", "2001", "1986"],
 			"image": "assets/images/Baby_Harry.jpg",
 
 		},
-		"questionTwenty":{
+		{
 			"question": "What does NEWT stand for?",
 			"chosen": false,
 			"correct": "Nastily Exhausting Wizarding Test",
-			"wrongOne": "NeverEnding Wizard Trials",
-			"wrongTwo": "Nauseating Exam for Wizard Talent",
-			"wrongThree": "No one remembers anymore, it was created so long ago.",
+			"wrong": ["NeverEnding Wizard Trials", "Nauseating Exam for Wizard Talent", "No one remembers anymore, it was created so long ago."],
 			"image": "assets/images/Lockhart's_Test.jpg",
 
 		}
-
-	},
+	],
 	"started":false,
 	"finished":false,
 	"answered":false,
-	"Questions":[],
+	"questions":[],
+
+//game start function
+	gameStart: function(){
+		$("#trivia").prepend("<h2>Time: <span id='timer'>30</span> Seconds Left</h2");
+		$("#start").addClass("hidden");
+		$("#question").removeClass("hidden");
+		$("#progress").removeClass("hidden");
+		game.setQuestions();
+		game.loadQuestion();
+	},
+
+//makes sure not to repeat questions
+	containsQuestion: function(qCheck, questions){
+	    for (var i = 0; i < questions.length; i++) {
+	        if (questions[i] === qCheck) {
+	            return true;
+	        }
+	    }
+	    return false;
+	},
+
+
+//Sets the question from the list 
+	randomQuestion: function(questionList){
+		if (questionList == game.hardQuestions){
+			var tempQID = Math.floor(Math.random()*4);
+		}
+		else{
+			var tempQID = Math.floor(Math.random()*8);
+		}
+		if (game.containsQuestion(questionList[tempQID], game.questions)){
+			game.randomQuestion(questionList);
+		}
+		else{
+			game.questions.push(questionList[tempQID]);
+		}
+		
+	},
+
+	setQuestions: function(){
+		var questionDiff = [game.easyQuestions, game.mediumQuestions, game.hardQuestions];
+		for (var i = 0; i<2;i++){
+			for (var j = 0; j < 4; j++){
+				game.randomQuestion(questionDiff[i]);
+			}	
+		}
+		for (var i = 0; i<2;i++){
+			game.randomQuestion(questionDiff[2]);
+		}
+		console.log(game.questions);
+	},
+
+	loadQuestion: function(){
+		interval = setInterval(game.timeCount, 1000);
+		$("#question").html("<h2>" + game.currentQuestion[0].question + "<h2>");
+	},
+
+//timer initialize function
+	timeCount: function(){
+		game.timer--;
+		$("#timer").html(game.timer);
+		if (game.timer ===0){
+			console.log("Time expired");
+			game.results();
+		}
+	},
+
+	results:function(){
+		if (game.timer <=0){
+			interval = clearInterval();
+			game.timeUp();
+		}
+		else{
+
+		}
+	},
+
+	timeUp: function(){
+		$("#question").html("You are out of time!")
+		$("#trivia").append("<button id=next-question>Next Question</button>")
+	}
 }
+
+//Event Listeners
+$(document).on("click", "#start-btn", function(){
+	game.gameStart();
+});
